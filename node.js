b4e91f419bec7bea1b3de1391cf3f77cc212605c
Henry Lad <113623013+HenryLad@users.noreply.github.com>
2024-02-14 19:41:29 +0100

    update
    updated the node.js and working on the redesgin of the index.html. Also add the ath.html were i'm playing on a feature where u can add athetles to count there points

--- node.js
+++ node.js
@@ -1,78 +1,84 @@
 function Header() {
    var header = document.getElementById("Header");
    var HeaderInput = document.getElementById("HeaderInput");
+   var selcetion = document.getElementById("Selection");
    
-   if (HeaderInput.value == "") {
+   if (selcetion.value == "") {
       alert("Please enter a header");
       return;
    }
    
-   var validEvents = ["100m Sprint", "Weitsprung", "Kugelstoßen", "Hochsprung", "400m Sprint", "110m Hürden", "Diskus", "Stabhochsprung", "Speerwurf", "1500m Lauf"];
+   var validEvents = ["100m Sprint (Sekunden)", "Weitsprung (Zentimeter)", "Kugelstoßen (Meter)", "Hochsprung (Zentimeter)", "400m Sprint (Sekunden)", "110m Hürden (Sekunden)", "Diskus (Meter)", "Stabhochsprung (Zentimeter)", "Speerwurf (Meter)", "1500m Lauf (Sekunden)"];
    
-   if (!validEvents.includes(HeaderInput.value)) {
+   if (!validEvents.includes(selcetion.value)) {
       alert("Please enter a valid event");
       return;
    }
    header.innerHTML = `
-   <h1>${HeaderInput.value}</h1>
+   <h1>${selcetion.value}</h1>
    `;
 }
+function selectAsHeaderInput(element) {
+   var HeaderInput = document.getElementById("HeaderInput");
+   var selcetion = document.getElementById("Selection");
+   selcetion.value = element.innerText;
+ }
 function CalPoints100() {
    var pointList = document.getElementById("pointList");
    var calInput = document.getElementById("calInput");
-   var headerinput = document.getElementById("HeaderInput");
+   var headerinput = document.getElementById("Selection");
 
    if (calInput.value == "") {
       alert("Please enter a time");
       return;
    }
    switch(headerinput.value) {
-      case"100m Sprint":
+      case"100m Sprint (Sekunden)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForRunning(25.4347, 18.0, 1.81, calInput.value)}</li>
       `;
       break;
-      case"Weitsprung":
+      case"Weitsprung (Zentimeter)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForJumpThrow(0.14354, 220, 1.40, calInput.value)}</li>
       `;
       break;
-      case"Kugelstoßen":
+      case"Kugelstoßen (Meter)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForJumpThrow(51.39, 1.5, 1.05, calInput.value)}</li>
       `;
       break;
-      case"Hochsprung":
+      case"Hochsprung (Zentimeter)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForJumpThrow(0.8465, 75, 1.42, calInput.value)}</li>
       `;
       break;
-      case"400m Sprint":
+      case"400m Sprint (Sekunden)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForRunning(1.53775, 82, 1.81, calInput.value)}</li>
       `;
       break;
-      case"110m Hürden":
+      case"110m Hürden (Sekunden)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForRunning(5.74352, 28.5, 1.92, calInput.value)}</li>
       `;
       break;
-      case"Diskus":
+      case"Diskus (Meter)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForJumpThrow(12.91, 4.0, 1.1, calInput.value)}</li>
       `;
       break;
-      case"Stabhochsprung":
+      case"Stabhochsprung (Zentimeter)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForJumpThrow(0.2797, 100, 1.35, calInput.value)}</li>
       `;
       break;
-      case"Speerwurf":
+      case"Speerwurf (Meter)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForJumpThrow(10.14, 7.0, 1.08, calInput.value)}</li>
       `;
       break;
-      case"1500m":
+      case"1500m Lauf (Sekunden)":
       pointList.innerHTML = `
       <li>The Athlete achvied a total of ${calculatePointsForRunning(0.03768, 480, 1.85, calInput.value)}</li>
       `;
